e123d871c515ef1b7238910e60ec829cfe3697f2
Peter Bangert <petbangert@gmail.com>
2021-07-13 15:39:12 +0200

    index.html,js,css: frontend V0.1.1

--- public/assets/js/interactive.js
+++ public/assets/js/interactive.js
@@ -1,25 +1,121 @@
 /*! jQuery v1.12.4 | (c) jQuery Foundation | jquery.org/license */
 
-$(document).ready(function(){
-$("button").click(function(e) {
-    var operation = this.id;
-    var signal = $(this).parent().parent().attr('id');
-    var source = $( "#sourceSelection option:selected" ).text();
-    e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:5000/api/v1/control",
-        data: {
-            "operation": operation,
-            "signal": signal,
-            "source":source
-        },
-        success: function(result) {
-            console.log(result);
-        },
-        error: function(result) {
-            console.log(result);
-        }
+$(document).ready(function () {
+    $(".cntrl-btn").click(function (e) {
+        var operation = this.id;
+        var signal = $(this).parent().parent().attr('id');
+        var source = $("#sourceSelection").find(".btn-primary").text();
+        console.log(operation);
+        console.log(signal);
+        console.log(source);
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:5000/api/v1/control",
+            data: {
+                "operation": operation,
+                "signal": signal,
+                "source": source
+            },
+            success: function (result) {
+                console.log(result);
+            },
+            error: function (result) {
+                console.log(result);
+            }
+        });
     });
 });
+
+$(document).ready(function () {
+    $('#submit').click( function(e) {
+        var name = $("#exampleFormControlInput1").val();
+        if (name === '') {
+            alert("Please input name before submitting");
+            return;
+        }
+
+        console.log(name);
+        console.log(randomized);
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:5000/api/v1/submit",
+            data: {
+                "name": name,
+                "guess": randomized,
+
+            },
+            success: function (result) {
+                console.log(result);
+            },
+            error: function (result) {
+                console.log(result);
+            }
+        });
+    });
 });
+
+$(document).ready(function () {
+    $('.source-select').on('click', '.btn-lg', function() {
+        $(this).addClass('btn-primary').siblings().removeClass('btn-primary').addClass('btn-default');
+    });
+});
+
+var list = document.getElementById('list')
+var base, randomized, dragging, draggedOver;
+var isRight = 'Not In Order!';
+
+const genRandom = (array) => {
+    base = array.slice()
+    randomized = array.sort(() => Math.random() - 0.5)
+    if (randomized.join("") !== base.join("")){
+        renderItems(randomized)
+    } else {
+        //recursion to account if the randomization returns the original array
+        genRandom()
+    }
+}
+
+const renderItems = (data) =>{
+    //document.getElementById('isRight').innerText = isRight
+    list.innerText = ''
+    data.forEach(item=>{
+        var node = document.createElement("li");
+        node.draggable = true
+        node.classList.add("list-group-item");
+        node.style.backgroundColor = item
+        node.style.backgroundColor = node.style.backgroundColor.length > 0
+            ? item : 'lightblue'
+        node.addEventListener('drag', setDragging)
+        node.addEventListener('dragover', setDraggedOver)
+        node.addEventListener('drop', compare)
+        node.innerText = item
+        list.appendChild(node)
+    })
+}
+
+const compare = (e) =>{
+    var index1 = randomized.indexOf(dragging);
+    var index2 = randomized.indexOf(draggedOver);
+    randomized.splice(index1, 1)
+    randomized.splice(index2, 0, dragging)
+
+    isRight = randomized.join("") === base.join("")
+        ? 'In Order!': 'Not In Order!'
+
+    renderItems(randomized)
+};
+
+
+const setDraggedOver = (e) => {
+    e.preventDefault();
+    draggedOver = Number.isNaN(parseInt(e.target.innerText)) ? e.target.innerText : parseInt(e.target.innerText)
+}
+
+const setDragging = (e) =>{
+    dragging = Number.isNaN(parseInt(e.target.innerText)) ? e.target.innerText : parseInt(e.target.innerText)
+}
+
+// genRandom([0, 1, 2, 3, 4, 5, 6])
+genRandom(['Peter', 'Ben', 'Roman', 'Valentin', 'Simon', 'Laurin', 'Christian','Luzie'])
