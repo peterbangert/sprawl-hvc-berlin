0c2857c74c6ce209b8a61ebc697df434d3948798
Peter Bangert <petbangert@gmail.com>
2021-07-14 12:24:27 +0200

    deploy,deploy_nginx: create new deploy script

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -7,9 +7,3 @@ cp nginx/sprawl-hvc-berlin /etc/nginx/sites-enabled/
 echo "Reloading Nginx ..."
 # Reload Nginx
 systemctl reload nginx.service
-
-echo "Starting Backend"
-# Start Flask
-python3 -m flask run --host=0.0.0.0 &
-
-#sclang simple_SERVER.sc
