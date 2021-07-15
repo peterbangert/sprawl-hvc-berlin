51db52b1085d06bfd6a341b5b31674ced41b44f2
Peter Bangert <petbangert@gmail.com>
2021-07-15 22:42:29 +0200

    changes

--- public/assets/js/results.js
+++ public/assets/js/results.js
@@ -1,25 +1,25 @@
 
   
 $(document).ready(function () {
-    
+
     $.ajax({
         type: "GET",
         url: "/api/v1/results",
         success: function (result) {
             console.log(result);
+            var data = [
+                {
+                    x: result.names,
+                    y: result.scores,
+                    type: 'bar'
+                }
+                ];
+                
+            Plotly.newPlot('results-div', data);
         },
         error: function (result) {
             console.log(result);
         }
     });
 
-    var data = [
-        {
-            x: result.names,
-            y: result.scores,
-            type: 'bar'
-        }
-        ];
-        
-    Plotly.newPlot('results-div', data);
 });
