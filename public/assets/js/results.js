7fd76398998e98415d24d9df3e590a4edf74ba34
Peter Bangert <petbangert@gmail.com>
2021-07-15 22:24:52 +0200

    results: add results page

---
+++ public/assets/js/results.js
@@ -0,0 +1,25 @@
+
+  
+$(document).ready(function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: "GET",
+        url: "/api/v1/results",
+        success: function (result) {
+            console.log(result);
+        },
+        error: function (result) {
+            console.log(result);
+        }
+    });
+
+    var data = [
+        {
+            x: ['giraffes', 'orangutans', 'monkeys'],
+            y: [20, 14, 23],
+            type: 'bar'
+        }
+        ];
+        
+        Plotly.newPlot('results-div', data);
+});
